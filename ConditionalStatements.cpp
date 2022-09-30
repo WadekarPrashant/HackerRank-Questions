af87e36c9f3318543b74eee0057211e57da61409
WadekarPrashant <wadekarprashant733@gmail.com>
2022-09-30 23:38:43 +0530

    conditionalstatements

---
+++ ConditionalStatements.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int N;
+    cin >> N;
+    if (N == 1) {
+        cout << "one" << endl;
+    } else if (N == 2) {
+        cout << "two" << endl;
+    } else if (N == 3) {
+        cout << "three" << endl;
+    } else if (N == 4) {
+        cout << "four" << endl;
+    } else if (N == 5) {
+        cout << "five" << endl;
+    } else if (N == 6) {
+        cout << "six" << endl;
+    } else if (N == 7) {
+        cout << "seven" << endl;
+    } else if (N == 8) {
+        cout << "eight" << endl;
+    } else if (N == 9) {
+        cout << "nine" << endl;
+    } else {
+        cout << "Greater than 9" << endl;
+    }
+ 
+   return 0;
+}
